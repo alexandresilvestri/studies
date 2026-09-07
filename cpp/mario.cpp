6f4c2e3622ec00dd5c65429d4a9cf75c9faed421
Alexandre Silvestri <airtontunni@gmail.com>
2026-09-07 12:30:47 -0300

    leetcode(refactor): inverted stair

--- cpp/mario.cpp
+++ cpp/mario.cpp
@@ -2,16 +2,16 @@
 #include <string>
 
 int getStairSize(void);
-std::string getSize(int size, int layers, char simbol);
+std::string getSize(int size, char simbol);
 
 int main() {
   const int layers = getStairSize();
 
   for (int i{1}; i < layers+1; i++) {
-    std::string empties{getSize(layers-i, layers, ' ')};
-    std::string hashtags{getSize(i, layers, '#')};
+    std::string empties{getSize(layers-i, ' ')};
+    std::string hashtags{getSize(i, '#')};
 
-    std::cout << empties << hashtags << std::endl;
+    std::cout << empties << hashtags << '\n';
   }
 
   return 0;
@@ -28,14 +28,6 @@ int getStairSize(void) {
   return number;
 }
 
-std::string getSize(int size, int layers, char simbol) {
-  int counter{1};
-  const char value{simbol};
-  std::string stack{};
-
-    for (int k{0}; k < size; k++) {
-      stack += value;
-    }
-
-  return stack;
+std::string getSize(int size, char simbol) {
+  return std::string(size, simbol);
 }
